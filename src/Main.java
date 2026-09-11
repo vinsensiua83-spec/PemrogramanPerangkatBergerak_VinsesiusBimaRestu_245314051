9c4ad340d8f441f41a4217630397b077df9ad60f
Bima <vinsensiua83@gmail.com>
2026-09-11 22:18:34 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Menyimpan biodata mahasiswa
+        String nama = "Andi Saputra";
+        int umur = 19;
+        double tinggiBadan = 168.5;
+        boolean mahasiswaAktif = true;
+        char golonganDarah = 'B';
+
+        // Menampilkan biodata
+        System.out.println("Nama            : " + nama);
+        System.out.println("Umur            : " + umur + " tahun");
+        System.out.println("Tinggi badan    : " + tinggiBadan + " cm");
+        System.out.println("Mahasiswa aktif : " + mahasiswaAktif);
+        System.out.println("Golongan darah  : " + golonganDarah);
+    }
+}
