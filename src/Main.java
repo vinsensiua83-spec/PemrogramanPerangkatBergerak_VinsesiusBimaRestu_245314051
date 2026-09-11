9503f05d78dd7d93b1b04d2df1aae2a9d1ee5e0f
Bima <vinsensiua83@gmail.com>
2026-09-11 22:20:18 +0700

    ganti main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Menyimpan biodata mahasiswa
-        String nama = "Andi Saputra";
+        String nama = "Budi Santoso";
         int umur = 19;
         double tinggiBadan = 168.5;
         boolean mahasiswaAktif = true;
